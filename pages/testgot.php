a2e59bc0553f6079f33267648d0e685444587bff
gotnatsarin <81910303+gotnatsarin@users.noreply.github.com>
2022-03-28 15:42:57 +0700

    g

--- pages/testgot.php
+++ pages/testgot.php
@@ -1,75 +1,46 @@
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="../assets/font-awesome-4.7.0/css/font-awesome.min.css">
-  <title>Venice</title>
-  <?php
-  require('../boostrap5css.php');
-  require('../boostrap5JS.php');
-  ?>
+  <title>testPrest</title>
+
+  <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
 </head>
 
 <body>
-  <div class="offcanvas offcanvas-start w-25" tabindex="-1" id="offcanvas" data-bs-keyboard="false" data-bs-backdrop="false">
-    <div class="offcanvas-header">
-      <h6 class="offcanvas-title d-none d-sm-block" id="offcanvas">Menu</h6>
-      <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-    </div>
-    <div class="offcanvas-body px-0">
-      <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-start" id="menu">
-        <li class="nav-item">
-          <a href="#" class="nav-link text-truncate">
-            <i class="fs-5 bi-house"></i><span class="ms-1 d-none d-sm-inline">Home</span>
-          </a>
-        </li>
-        <li>
-          <a href="#submenu1" data-bs-toggle="collapse" class="nav-link text-truncate">
-            <i class="fs-5 bi-speedometer2"></i><span class="ms-1 d-none d-sm-inline">Dashboard</span> </a>
-        </li>
-        <li>
-          <a href="#" class="nav-link text-truncate">
-            <i class="fs-5 bi-table"></i><span class="ms-1 d-none d-sm-inline">Orders</span></a>
-        </li>
-        <li class="dropdown">
-          <a href="#" class="nav-link dropdown-toggle  text-truncate" id="dropdown" data-bs-toggle="dropdown" aria-expanded="false">
-            <i class="fs-5 bi-bootstrap"></i><span class="ms-1 d-none d-sm-inline">Bootstrap</span>
-          </a>
-          <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdown">
-            <li><a class="dropdown-item" href="#">New project...</a></li>
-            <li><a class="dropdown-item" href="#">Settings</a></li>
-            <li><a class="dropdown-item" href="#">Profile</a></li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li><a class="dropdown-item" href="#">Sign out</a></li>
-          </ul>
-        </li>
-        <li>
-          <a href="#" class="nav-link text-truncate">
-            <i class="fs-5 bi-grid"></i><span class="ms-1 d-none d-sm-inline">Products</span></a>
-        </li>
-        <li>
-          <a href="#" class="nav-link text-truncate">
-            <i class="fs-5 bi-people"></i><span class="ms-1 d-none d-sm-inline">Customers</span> </a>
-        </li>
-      </ul>
-    </div>
-  </div>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col min-vh-100 py-3">
-        <!-- toggler -->
-        <button class="btn float-end" data-bs-toggle="offcanvas" data-bs-target="#offcanvas" role="button">
-          <i class="bi bi-arrow-right-square-fill fs-3" data-bs-toggle="offcanvas" data-bs-target="#offcanvas"></i>
-        </button>
-        Content..
-      </div>
-    </div>
-  </div>
+  <input type="text" name="test" id="test">
+  <button type="button" id="submit">บันทึก</button>
+
+  <script>
+  $(document).ready(function() {
+    const Perth = () => {
+      let test = $("#test").val()
+      $.ajax({
+        type: "POST"
+        url: "testgot2.php"
+        data: {
+          test: test
+        },
+        success: function(data) {
+          Swal.fire({
+            icon: 'success',
+            title: 'สำเร็จ',
+            text: data,
+          })
+        },
+      });
+    };
+    $("#submit").click(function() {
+      Perth()
+    });
+  });
+  </script>
+
 </body>
 
 </html>
