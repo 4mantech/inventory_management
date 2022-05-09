13314fcb51c3f8ed352be6dea49061ec0bf64acc
tangza0555@gmail.com <tangza0555@gmail.com>
2022-05-10 00:05:21 +0700

    delete report

--- pages/query/deleteReport.php
+++ pages/query/deleteReport.php
@@ -4,13 +4,15 @@ require('connect.php');
 
 $sqlCheck = "SELECT * FROM reports WHERE id = '$id'";
 $resultCheck = mysqli_query($conn,$sqlCheck);
-$rows = mysqli_fetch_array($resultCheck,MYSQLI_ASSOC);
+$row = mysqli_fetch_array($resultCheck,MYSQLI_ASSOC);
 
 if(mysqli_num_rows($resultCheck)== 1){
   $sql = "DELETE FROM reports WHERE id = '$id'";
-  if($result = mysqli_query($conn,$sql)){
-    unlink('../../assets/images/reports/'.$rows['imageName']);
-    echo json_encode(array("status"=>"true"),JSON_UNESCAPED_UNICODE);
+  if(mysqli_query($conn,$sql)){
+    if($row['imageName'] != "noImage.jpg"){
+      unlink('../../assets/images/reports/'.$row['imageName']);
+    }
+  echo json_encode(array("status"=>"true"),JSON_UNESCAPED_UNICODE);
   }
 }else {
   echo json_encode(array("status"=>"false"),JSON_UNESCAPED_UNICODE);
